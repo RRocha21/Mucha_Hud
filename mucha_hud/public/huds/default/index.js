476e9cee068161633203c8b8551e0ee643a1f2ac
G2-Rocha <132342030+G2-Rocha@users.noreply.github.com>
2024-01-08 13:46:51 +0000

    fix/win_line

--- mucha_hud/public/huds/default/index.js
+++ mucha_hud/public/huds/default/index.js
@@ -2605,8 +2605,10 @@ function updatePage(data) {
 
         function stopAnimationWinner() {
 
-            $(".win_container_left").css("transform", "translateX(270px)").css("transition", "all 0.8s ease 0.4s");
-            $(".win_container_right").css("transform", "translateX(-270px)").css("transition", "all 0.8s ease 0.4s");
+            $(".win_container_left").css("transform", "translateX(250px)").css("transition", "all 0.8s ease 0.4s");
+            $(".win_container_right").css("transform", "translateX(-250px)").css("transition", "all 0.8s ease 0.4s");
+            $(".win_container_left").css("opacity", "0");
+            $(".win_container_right").css("opacity", "0");
             $(".win_text_left").css("transform", "translateY(-50px)").css("transition", "all 0.6s ease 0.1s");
             $(".win_text_right").css("transform", "translateY(-50px)").css("transition", "all 0.6s ease 0.1s");
             $(".win_text_right").css("opacity", "0").css("transition", "all 1s ease 0.6s");
